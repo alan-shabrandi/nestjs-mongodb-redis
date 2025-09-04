6119074e0a0a0eea84e0490f3f3715c6e423bbfe
Alan Shabrandi <alan.shabrandi@gmail.com>
2025-09-04 15:56:02 +0330

    create user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './schemas/user.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(
+    @Body() body: { name: string; email: string; password: string },
+  ): Promise<User> {
+    return this.usersService.createUser(body.name, body.email, body.password);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(
+    name: string,
+    email: string,
+    password: string,
+  ): Promise<User> {
+    const newUser = new this.userModel({ name, email, password });
+    return newUser.save();
+  }
+}
